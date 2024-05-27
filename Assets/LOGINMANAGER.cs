b62cb6713d29a08ec374cf484cc3d81b21bbc699
LautaroZayat <148563150+LautaroZayat@users.noreply.github.com>
2024-05-27 14:26:22 -0300

    cambios 2

---
+++ Assets/LOGINMANAGER.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LOGINMANAGER : MonoBehaviour
+{
+
+    public InputField Inputcontraseña;
+
+    public string contraseña = "hola";
+
+    public void Button_Pressed()
+    {
+        if (Inputcontraseña.text == contraseña)
+        {
+            Debug.Log("Access granted");
+        }
+        else 
+        {
+            Debug.Log("Access denied");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
